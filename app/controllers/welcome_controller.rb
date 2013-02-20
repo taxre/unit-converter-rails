66e0df34b2146884778af2174307c48a0dc6af3a
Lee Quarella <lee@lucidfrog.com>
2013-02-20 11:01:37 -0500

    Updated app for use with unit_converter_gem v 0.1.0

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,14 @@
 class WelcomeController < ApplicationController
- 
   def convert
-    amount = params[:unit_1].to_f
-    from_unit = params[:unit_type_1]
-    to_unit = params[:unit_type_2]
+    @amount = params[:amount].to_f
+    @from_unit = params[:from_unit].downcase
+    @to_unit = params[:to_unit].downcase
     begin
       @converted = UnitConverter
-        .convert(amount, :"#{from_unit}", :"#{to_unit}")
-    rescue UnkownConversionError
-      @error = "Sorry, I don't know how to convert #{from_unit} to 
-      #{to_unit}."
+        .convert(@amount, :"#{@from_unit}", :"#{@to_unit}")
+    rescue UnitConverter::UnknownConversionError
+      @error = "Sorry, I don't know how to convert #{@from_unit} to 
+      #{@to_unit}."
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 UnitConverterWebapp::Application.routes.draw do
   root :to => 'welcome#index'
 
-  match "/distances" => "distances#index", :as => "distances"
-  match "/distances/convert" => "distances#convert", :as => "convert_distance"
-
-  match "/weights" => "weights#index", :as => "weights"
-  match "/weights/convert" => "weights#convert", :as => "convert_weight"
-
-  match "/temparatures" => "temperatures#index", :as => "temperatures"
-  match "/temperatures/convert" => "temperatures#convert", :as => "convert_temperature"
+  match "/convert" => "welcome#convert", :as => "convert"
 end
